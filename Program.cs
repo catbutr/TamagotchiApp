4643df7e361eef1a09bce8f8c8bc3efb7a70d6e9
catbutr <pkarabatov@gmail.com>
2024-03-12 14:42:26 +0700

    Basic skeleton menu

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using TamagotchiApp;
-
 Console.WriteLine("Write down you tamogochi's name!");
-string? petName = Console.ReadLine();
+var isMenuEnded = false;
+var petName = Console.ReadLine();
 if (petName == null)
 {
     Console.WriteLine("Pet name cannot be empty!");
 }
 Pet newPet = new Pet(petName);
 newPet.ConsoleOutput();
+while (isMenuEnded == false)
+{
+    Console.WriteLine("Choose the command:");
+    Console.WriteLine("1:Feed");
+    Console.WriteLine("2:Play");
+    Console.WriteLine("3:Sleep");
+    var inputCommand = Console.ReadLine();
+    var inputCommandInt = Convert.ToInt32(inputCommand);
+    switch (inputCommandInt)
+    {
+        case 1:newPet.Feed(); newPet.ConsoleOutput();
+            break;
+        case 2:newPet.Play();newPet.ConsoleOutput();
+            break;
+        case 3:newPet.Play(); newPet.ConsoleOutput();
+            break;
+        default: Console.WriteLine("Incorrect Command"); break;
+    }
+}
